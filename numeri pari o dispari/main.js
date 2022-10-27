8b468e8ed3be9fb80765bda3dd73b342633a7b8d
Maino24 <mg02.gabriele@gmail.com>
2022-10-27 22:54:01 +0200

    faccio inserire un numero

--- numeri pari o dispari/main.js
+++ numeri pari o dispari/main.js
@@ -4,3 +4,12 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
+
+let numeroInput = parseInt(promt(`inserisci un numero compreso tra 1 e 5`));
+
+if(numeroInput > 5){
+
+    let numeroInput = parseInt(promt(`inserisci un numero compreso tra 1 e 5`));
+
+
+}

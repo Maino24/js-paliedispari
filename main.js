ee0668ab77ae4f74c3441890d3da1de20b1c10c9
Maino24 <mg02.gabriele@gmail.com>
2022-10-27 17:52:05 +0200

    chiedo di inserire una parola

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 /*Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
+
+
+//chiedo di inserire una parola
+let parolaInput = prompt(`inserisci una parola`);

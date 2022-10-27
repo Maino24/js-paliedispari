02ecdafa162029d28b098a1b89b3acdf721f3f67
Maino24 <mg02.gabriele@gmail.com>
2022-10-27 23:15:07 +0200

    creata funzione random e funzione pariDispari

--- numeri pari o dispari/main.js
+++ numeri pari o dispari/main.js
@@ -5,11 +5,34 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
 
-let numeroInput = parseInt(promt(`inserisci un numero compreso tra 1 e 5`));
+let numeroInput = parseInt(prompt(`inserisci un numero compreso tra 1 e 5`));
+let somma = 0
+console.log(numeroInput)
 
-if(numeroInput > 5){
-
-    let numeroInput = parseInt(promt(`inserisci un numero compreso tra 1 e 5`));
-
+//genero un numero random attraverso una funzione
+function random (){
+    numeroRandom = Math.floor(Math.random() * 5) + 1
+    somma = somma + numeroRandom
+    console.log(numeroRandom)
 
 }
+
+random()
+
+//sommo numero utente e numero random
+sommaTot = numeroInput + somma
+console.log(sommaTot)
+
+
+function pariDispari(){
+
+    if(sommaTot % 2 !== 0){
+        console.log(`è dispari`)
+    }else{
+        console.log(`è pari`)
+    }
+
+}
+
+
+pariDispari()

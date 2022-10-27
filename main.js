dbe0d799f330e4efe2ef0153063f596864a63800
Maino24 <mg02.gabriele@gmail.com>
2022-10-27 18:02:16 +0200

    funzione split

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 
 //chiedo di inserire una parola
 let parolaInput = prompt(`inserisci una parola`);
+
+
+i = 0;
+
+//con la funzione split divido la parola inserita in lettere con posizione differente in un array
+parolaDivisa = parolaInput.split(``)
+console.log(parolaDivisa)
+
+parolaDivisa = parolaDivisa.reverse();
+console.log(``)
+
+parolaContrario = parolaDivisa.join(``);
+console.log(parolaContrario);
